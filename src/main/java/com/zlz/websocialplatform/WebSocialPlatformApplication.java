6c16c4cc81d78606d60adb5c6d50b96f25bbb4c2
PulsarShock <2418754520@qq.com>
2022-04-11 23:47:37 +0800

    设置好目录结构

---
+++ src/main/java/com/zlz/websocialplatform/WebSocialPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.zlz.websocialplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocialPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocialPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlz/websocialplatform/WebSocialPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlz.websocialplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocialPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
